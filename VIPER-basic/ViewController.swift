164cf0ef1bf8fcfceadcd44e143a0013014d20d9
Ziterz <ziady.mubaraq@gmail.com>
2024-02-27 22:31:21 +0700

    Initial Commit

---
+++ VIPER-basic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER-basic
+//
+//  Created by Ziady Mubaraq on 27/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
